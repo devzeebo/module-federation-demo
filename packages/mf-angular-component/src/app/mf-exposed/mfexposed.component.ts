a58f1f76e6908faac3e1dba9cf57849f8a6a0bea
Will Hausman <will.jhausman@outlook.com>
2023-10-21 21:47:36 -0500

    custom events and inputs

--- packages/mf-angular-component/src/app/mf-exposed/mfexposed.component.ts
+++ packages/mf-angular-component/src/app/mf-exposed/mfexposed.component.ts
@@ -1,10 +1,25 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   standalone: true,
   encapsulation: ViewEncapsulation.ShadowDom,
   selector: 'app-exposed-component',
-  template: '<div>Some ng component.</div>',
+  template: '<div>Some ng component with {{someInput}}. Count: {{count}} <button (click)="onClick()">Up</button></div>',
 })
 export class MfExposedComponent {
+  @Input({ required: true })
+  someInput!: string;
+
+  constructor(private element: ElementRef) {
+    this.element.nativeElement.addEventListener('increment', (e: CustomEvent) => console.log('from angular', e));
+  }
+
+  count = 0;
+  onClick() {
+    this.count++;
+
+    this.element.nativeElement.dispatchEvent(new CustomEvent('increment', {
+      detail: this.count
+    }));
+  }
 }
--- packages/spa-app/src/components/App/App.tsx
+++ packages/spa-app/src/components/App/App.tsx
@@ -1,13 +1,24 @@
 import type { DOMAttributes } from 'react';
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 
 import { RepoChart } from '@devzeebo/mf-react-component/RepoChart';
 // import { AngularWrapper } from '@bubblydoo/angular-react';
 // import { MfExposedComponent } from '@devzeebo/mf-angular-component/mfexposed.component';
 import { Grid } from '../Grid';
 
-const App = () => (
-  <Grid style={{
+const App = () => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  useLayoutEffect(() => {
+    const onIncrement = (e: CustomEvent) => console.log('from react', e);
+
+    const { current } = ref as any;
+
+    current.addEventListener('increment', onIncrement);
+    return () => current.removeEventListener('increment', onIncrement);
+  });
+
+  return <Grid style={{
     minHeight: '100vh',
   }}
   >
@@ -27,12 +38,12 @@ const App = () => (
       <Grid fill style={{ padding: '1em', margin: '1em', border: '1px solid lightgray' }}>
         <span style={{ alignSelf: 'center' }}>Angular Component</span>
         <Grid fill style={{ justifyContent: 'center', alignItems: 'center' }}>
-          <mf-exposed-component />
+          <mf-exposed-component ref={ref} some-input="simple text" />
         </Grid>
       </Grid>
     </Grid>
   </Grid>
-);
+};
 export default App;
 
 type CustomElement<T> = Partial<T & DOMAttributes<T> & { children: any }>;
